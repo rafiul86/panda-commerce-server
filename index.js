80551f95b02caecc91126e32e596cbddf6d0ed8e
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-04-09 20:50:42 +0600

    search option added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3be27.mongodb.net/vandb?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! What is the main focus of the day !!'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }))
 
 client.connect(err => {
   const collection = client.db("vandb").collection("van");
-
+  const clientDataCollection = client.db("vandb").collection("panda");
   app.post('/reviewProduct',(req,res)=>{
       const productKeys = req.body;
       collection.find({key : {$in : productKeys}})
@@ -33,7 +33,8 @@ client.connect(err => {
     })
 })
     app.get('/products',(req,res)=>{
-        collection.find({})
+        const search = req.query.search
+        collection.find({name : {$regex : search }})
         .toArray((err, documents)=>{
             res.send(documents)
         })
@@ -43,6 +44,13 @@ client.connect(err => {
         collection.insertMany(products)
         .then(result =>{
             res.send(result)
+        })
+    })
+    app.post('/addOrders',(req,res)=>{
+        const orderInfo = req.body ;
+        clientDataCollection.insertOne(orderInfo)
+        .then(result =>{
+            res.send(result.insertedCount > 0)
         })
     })
   console.log('database connected')
